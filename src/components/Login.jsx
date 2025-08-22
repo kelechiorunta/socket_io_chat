4427bd42b5497071180e91423ccf22bba24a2f17
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-22 17:26:29 +0100

    feat: implement animated login greeting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -58,10 +58,10 @@ export default function Login() {
   return (
     <Container
       className="d-flex flex-column align-items-center justify-content-center"
-      style={{ minHeight: '100vh', maxWidth: '500px' }}
+      style={{ minHeight: '100vh', maxWidth: '500px', backgroundImage: 'url(/screenshot.png)' }}
     >
-      <Row className="w-100 shadow p-4 rounded bg-dark">
-        <AnimateText texts={['J U S T C H A T', "Let's Chat"]} />
+      <Row className="w-100 shadow p-2 rounded bg-dark">
+        <AnimateText texts={['J U S T C H A T', "Let's Chat", 'Connect with Pals']} />
       </Row>
       <Row className="w-100 shadow p-4 rounded bg-white">
         <Col>
