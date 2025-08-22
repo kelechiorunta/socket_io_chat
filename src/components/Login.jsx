3a55b90fb8374881676cccea1ae27ab537847adf
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-22 15:35:27 +0100

    feat: implement animated login greeting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -99,7 +99,7 @@ export default function Login() {
 
                 <div className="d-grid mb-3">
                   <Button
-                    style={{ fontFamily: 'Raleway' }}
+                    style={{ fontFamily: 'Jost' }}
                     type="submit"
                     disabled={isSubmitting}
                     variant="primary"
@@ -113,7 +113,7 @@ export default function Login() {
                   <NavLink
                     href="/api/google"
                     style={{
-                      fontFamily: 'Raleway',
+                      fontFamily: 'Jost',
                       width: '100%',
                       borderRadius: '10px',
                       color: 'white'
