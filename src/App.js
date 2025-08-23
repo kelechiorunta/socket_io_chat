b5f28ab6aa69c3cfef455ca9f14ab28591b7fee8
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-23 14:09:49 +0100

    feat: implement forgot password logic and ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import Login from './components/Login.jsx';
 import SignUp from './components/SignUp.jsx';
+import ForgotPassword from './ForgotPassword/ForgotPassword.jsx';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       </Route>
 
       <Route path="/login" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
