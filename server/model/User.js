ac34753ece471bbe1ea5fe73e7512f9801f6732f
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-24 15:54:59 +0100

    feat: implement forgot password logic and ui

--- server/model/User.js
+++ server/model/User.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
   lastMessage: { type: String, required: false, default: '' },
   lastMessageCount: { type: Number, required: false, default: 0 },
   isOnline: { type: Boolean, required: false, default: null },
-  unread: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UnreadMsg' }],
+  unread: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UnreadMsg', required: false }],
   token: { type: String, required: false },
   birthday: { type: String, required: false },
   phone: { type: String, required: false },
@@ -24,19 +24,22 @@ const userSchema = new mongoose.Schema({
   address: { type: String, required: false },
   otp: {
     type: String,
-    default: 'otp'
+    default: 'otp',
+    required: false
     // required: true,
   },
   expireAt: {
     type: Date,
-    default: () => new Date(Date.now() + 300 * 1000) // Set to 5 minutes from now
+    default: () => new Date(Date.now() + 300 * 1000), // Set to 5 minutes from now
+    required: false
   },
-  resetPasswordToken: { type: String },
-  resetPasswordExpires: { type: Date },
+  resetPasswordToken: { type: String, required: false },
+  resetPasswordExpires: { type: Date, required: false },
   google: {
     name: { type: String, required: false },
     email: { type: String, required: false, validate: validator.isEmail },
-    accessToken: { type: String, required: false }
+    accessToken: { type: String, required: false },
+    active: { type: Boolean, required: false }
   }
   // Example schema update
   // unreadCounts: {
