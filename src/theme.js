3fbaa81dd262d656c225734ccf4135d5ec24dd3a
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-08-27 12:13:59 +0100

    fix: refactor ui to chakra ui

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,23 @@
 // theme.js
 const theme = {
-  initialColorMode: 'light',
-  useSystemColorMode: false
+  config: {
+    initialColorMode: 'light', // or 'dark'
+    useSystemColorMode: false
+  },
+  colors: {
+    brand: {
+      50: '#e0f2f1',
+      100: '#b2dfdb',
+      200: '#80cbc4',
+      300: '#4db6ac',
+      400: '#26a69a',
+      500: '#009688',
+      600: '#00897b',
+      700: '#00796b',
+      800: '#00695c',
+      900: '#004d40'
+    }
+  }
 };
 
 export default theme;
