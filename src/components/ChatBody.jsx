ad2f88aea33872bd619fc46f6036c4bdd697fc70
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-09 22:03:24 +0100

    feat: implement blur-loading with ProgressiveImage component

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -84,7 +84,7 @@ const ChatBody = ({ messages = [], pic, chat, typingUsers }) => {
 
         const isClient = msg?.sender?._id === pic?._id || msg.from === 'client';
 
-        // console.log('messages', msg);
+        console.log('messages', msg);
 
         return (
           <React.Fragment key={msg._id || index}>
