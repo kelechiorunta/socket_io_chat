2856173c22b59e4fc1a8735c9bb865c381048d22
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-10 05:19:55 +0100

    feat: implement blur-loading with ProgressiveImage component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -272,7 +272,7 @@ const Sidebar = ({
                 sx={{
                   mb: 1,
                   cursor: 'pointer',
-                  bgcolor: isSelected ? 'ThreeDLightShadow' : isDark ? 'white' : 'wheat', // : 'grey.200', //success.main
+                  bgcolor: isSelected ? 'ThreeDLightShadow' : isDark ? 'grey.800' : 'grey.200', //success.main
                   color: isSelected ? 'white' : isDark ? 'white' : 'black'
                 }}
                 style={{
