da954322b3a8292bc95cd8cff9c20abb92352247
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-13 11:28:18 +0100

    fix: fix filters in Sidebar

--- src/components/ChatFilters.jsx
+++ src/components/ChatFilters.jsx
@@ -7,20 +7,129 @@ export default function ChatFilters({ handleSort, isDark }) {
   const [tab, setTab] = useState('all');
 
   return (
+    // <Stack
+    //   direction="row"
+    //   spacing={2}
+    //   alignItems="center"
+    //   justifyContent="space-between"
+    //   sx={{
+    //     mb: 2,
+    //     fontSize: 14,
+    //     bgcolor: isDark ? 'grey.900' : 'grey.50',
+    //     color: isDark ? 'grey.100' : 'grey.900',
+    //     border: 1,
+    //     borderColor: isDark ? 'grey.700' : 'grey.300',
+    //     borderRadius: 2,
+    //     p: 1.5
+    //   }}
+    // >
+    //   {/* Filter */}
+    //   <FormControl
+    //     variant="outlined"
+    //     size="small"
+    //     sx={{
+    //       minWidth: 100,
+    //       '& .MuiOutlinedInput-root': {
+    //         color: isDark ? 'grey.100' : 'grey.900',
+    //         bgcolor: isDark ? 'grey.800' : 'white',
+    //         '& fieldset': {
+    //           borderColor: isDark ? 'grey.600' : 'grey.300'
+    //         },
+    //         '&:hover fieldset': {
+    //           borderColor: isDark ? 'grey.400' : 'grey.500'
+    //         }
+    //       },
+    //       '& .MuiInputLabel-root': {
+    //         color: isDark ? 'grey.300' : 'grey.700'
+    //       }
+    //     }}
+    //   >
+    //     <InputLabel>Filter</InputLabel>
+    //     <Select value={filter} label="Filter" onChange={(e) => setFilter(e.target.value)}>
+    //       <MenuItem value="all">All</MenuItem>
+    //       <MenuItem value="unread">Unread</MenuItem>
+    //       <MenuItem value="online">Online</MenuItem>
+    //     </Select>
+    //   </FormControl>
+
+    //   {/* Sort */}
+    //   <FormControl
+    //     size="small"
+    //     sx={{
+    //       minWidth: 100,
+    //       '& .MuiOutlinedInput-root': {
+    //         color: isDark ? 'grey.100' : 'grey.900',
+    //         bgcolor: isDark ? 'grey.800' : 'white',
+    //         '& fieldset': {
+    //           borderColor: isDark ? 'grey.600' : 'grey.300'
+    //         },
+    //         '&:hover fieldset': {
+    //           borderColor: isDark ? 'grey.400' : 'grey.500'
+    //         }
+    //       },
+    //       '& .MuiInputLabel-root': {
+    //         color: isDark ? 'grey.300' : 'grey.700'
+    //       }
+    //     }}
+    //   >
+    //     <InputLabel>Sort</InputLabel>
+    //     <Select value={sort} label="Sort" onChange={(e) => setSort(e.target.value)}>
+    //       <MenuItem value="newest">Newest</MenuItem>
+    //       <MenuItem value="oldest">Oldest</MenuItem>
+    //     </Select>
+    //   </FormControl>
+
+    //   {/* View */}
+    //   <FormControl
+    //     size="small"
+    //     sx={{
+    //       minWidth: 100,
+    //       '& .MuiOutlinedInput-root': {
+    //         color: isDark ? 'grey.100' : 'grey.900',
+    //         bgcolor: isDark ? 'grey.800' : 'white',
+    //         '& fieldset': {
+    //           borderColor: isDark ? 'grey.600' : 'grey.300'
+    //         },
+    //         '&:hover fieldset': {
+    //           borderColor: isDark ? 'grey.400' : 'grey.500'
+    //         }
+    //       },
+    //       '& .MuiInputLabel-root': {
+    //         color: isDark ? 'grey.300' : 'grey.700'
+    //       }
+    //     }}
+    //   >
+    //     <InputLabel>View</InputLabel>
+    //     <Select
+    //       value={tab}
+    //       label="View"
+    //       onChange={(e) => {
+    //         setTab(e.target.value);
+    //         if (e.target.value === 'contacts') {
+    //           handleSort();
+    //         }
+    //       }}
+    //     >
+    //       <MenuItem value="all">All Chats</MenuItem>
+    //       <MenuItem value="groups">Groups</MenuItem>
+    //       <MenuItem value="contacts">Contacts</MenuItem>
+    //     </Select>
+    //   </FormControl>
+    // </Stack>
     <Stack
       direction="row"
-      spacing={2}
+      spacing={1}
       alignItems="center"
       justifyContent="space-between"
       sx={{
         mb: 2,
-        fontSize: 14,
+        fontSize: { xs: 12, sm: 13, md: 14 }, // responsive font sizes
         bgcolor: isDark ? 'grey.900' : 'grey.50',
         color: isDark ? 'grey.100' : 'grey.900',
         border: 1,
         borderColor: isDark ? 'grey.700' : 'grey.300',
         borderRadius: 2,
-        p: 1.5
+        p: 1
       }}
     >
       {/* Filter */}
@@ -28,8 +137,9 @@ export default function ChatFilters({ handleSort, isDark }) {
         variant="outlined"
         size="small"
         sx={{
-          minWidth: 100,
+          minWidth: { xs: 80, sm: 100 }, // smaller on mobile
           '& .MuiOutlinedInput-root': {
+            fontSize: { xs: 12, sm: 13, md: 14 },
             color: isDark ? 'grey.100' : 'grey.900',
             bgcolor: isDark ? 'grey.800' : 'white',
             '& fieldset': {
@@ -40,12 +150,18 @@ export default function ChatFilters({ handleSort, isDark }) {
             }
           },
           '& .MuiInputLabel-root': {
+            fontSize: { xs: 12, sm: 13 },
             color: isDark ? 'grey.300' : 'grey.700'
           }
         }}
       >
         <InputLabel>Filter</InputLabel>
-        <Select value={filter} label="Filter" onChange={(e) => setFilter(e.target.value)}>
+        <Select
+          value={filter}
+          label="Filter"
+          onChange={(e) => setFilter(e.target.value)}
+          sx={{ fontSize: { xs: 12, sm: 13, md: 14 } }}
+        >
           <MenuItem value="all">All</MenuItem>
           <MenuItem value="unread">Unread</MenuItem>
           <MenuItem value="online">Online</MenuItem>
@@ -56,8 +172,9 @@ export default function ChatFilters({ handleSort, isDark }) {
       <FormControl
         size="small"
         sx={{
-          minWidth: 100,
+          minWidth: { xs: 80, sm: 100 },
           '& .MuiOutlinedInput-root': {
+            fontSize: { xs: 12, sm: 13, md: 14 },
             color: isDark ? 'grey.100' : 'grey.900',
             bgcolor: isDark ? 'grey.800' : 'white',
             '& fieldset': {
@@ -68,12 +185,18 @@ export default function ChatFilters({ handleSort, isDark }) {
             }
           },
           '& .MuiInputLabel-root': {
+            fontSize: { xs: 12, sm: 13 },
             color: isDark ? 'grey.300' : 'grey.700'
           }
         }}
       >
         <InputLabel>Sort</InputLabel>
-        <Select value={sort} label="Sort" onChange={(e) => setSort(e.target.value)}>
+        <Select
+          value={sort}
+          label="Sort"
+          onChange={(e) => setSort(e.target.value)}
+          sx={{ fontSize: { xs: 12, sm: 13, md: 14 } }}
+        >
           <MenuItem value="newest">Newest</MenuItem>
           <MenuItem value="oldest">Oldest</MenuItem>
         </Select>
@@ -83,8 +206,9 @@ export default function ChatFilters({ handleSort, isDark }) {
       <FormControl
         size="small"
         sx={{
-          minWidth: 100,
+          minWidth: { xs: 80, sm: 100 },
           '& .MuiOutlinedInput-root': {
+            fontSize: { xs: 12, sm: 13, md: 14 },
             color: isDark ? 'grey.100' : 'grey.900',
             bgcolor: isDark ? 'grey.800' : 'white',
             '& fieldset': {
@@ -95,6 +219,7 @@ export default function ChatFilters({ handleSort, isDark }) {
             }
           },
           '& .MuiInputLabel-root': {
+            fontSize: { xs: 12, sm: 13 },
             color: isDark ? 'grey.300' : 'grey.700'
           }
         }}
@@ -109,6 +234,7 @@ export default function ChatFilters({ handleSort, isDark }) {
               handleSort();
             }
           }}
+          sx={{ fontSize: { xs: 12, sm: 13, md: 14 } }}
         >
           <MenuItem value="all">All Chats</MenuItem>
           <MenuItem value="groups">Groups</MenuItem>
