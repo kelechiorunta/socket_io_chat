1ef94cb63794745edde72a4c2d2b224a0df740e2
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-15 00:05:06 +0100

    feat: implement drawer for Iconbar on mobile view

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -648,7 +648,7 @@ const ChatApp = () => {
             md={1}
             lg={1}
             style={{ position: 'sticky', maxWidth: 60 }}
-            className={`p-0 border-end chat-iconbar ${
+            className={`d-flex flex-column justify-content-center align-items-center p-2 border-end chat-iconbar ${
               mobileView === 'chat' ? 'd-none' : 'd-block d-lg-block'
             }`}
           >
