e03865551343ff4f984d30eaef53f6c441842e67
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-24 00:52:37 +0100

    fix: fix fetchGroup avatar streaming

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -223,7 +223,7 @@ const Sidebar = ({
         {/* ✅ Create Group Drawer */}
         <CreateGroupDrawer
           open={isCreateGroupOpen}
-          users={tab === 'contacts' || tab === 'all' ? filteredUsers : []}
+          users={tab === 'all' ? filteredUsers : []}
           onClose={() => setIsCreateGroupOpen(false)}
           isDark={isDark}
         />
