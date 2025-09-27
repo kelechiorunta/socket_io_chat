c1066d603ddecde5d39da312bf1a2045e2f3dc5e
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-09-27 23:42:07 +0100

    feat: implement fetch of group msgs

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -784,7 +784,7 @@ const ChatApp = () => {
             setIsIconBarOpen={setIsIconBarOpen}
             handleGroupChat={handleGroupChat}
             isDark={isDark}
-            groups={groupData.fetchData}
+            groups={groupData && groupData.fetchData}
             groupLoading={groupLoading}
             groupError={groupError}
             tab={tab}
