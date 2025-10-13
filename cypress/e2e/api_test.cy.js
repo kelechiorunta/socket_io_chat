5649f2e39c60d9059c512b63d8e41d508a8c5920
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-10-13 01:04:36 +0100

    fix: fix Message model for group messages fetch from resolver

--- cypress/e2e/api_test.cy.js
+++ cypress/e2e/api_test.cy.js
@@ -13,9 +13,20 @@ describe('fetch api testing', () => {
   it('calls /api/isAuthenticated', () => {
     // Confirms the window fetch makes a get request to the route /api/isAuthenticated
     cy.window().its('fetch').should('be.calledWith', '/api/isAuthenticated');
+    // Confirms the window fetch is called once
+    cy.window().its('fetch').should('have.been.calledOnce');
     // Sees the loader/spinner text while authenticating
     cy.get('div')
       .contains('Authenticating, please wait...', { matchCase: false })
       .should('be.visible');
+
+    cy.fixture('example').then((data) => {
+      cy.window().then((win) => {
+        //attaches fixture to browser window
+        win.data = data;
+      });
+    });
+
+    cy.window().its('data.email').should('eq', 'hello@cypress.io');
   });
 });
